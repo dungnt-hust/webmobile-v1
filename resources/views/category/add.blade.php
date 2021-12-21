2d4a3253374fb5db98d39254727ce1e884ecaca3
unknown <tiendung160301@gmail.com>
2021-12-21 21:33:54 +0700

    Initial commit

---
+++ resources/views/category/add.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+
+@section('title')
+    <title>Thêm sản phẩm</title>
+@endsection
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        @include('partials.content-header', ['name' => 'category', 'key' => 'Add'])
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-6">
+                      <form action="{{route('categories.store')}}" method="post">
+                          @csrf
+                          <div class="form-group">
+                              <label>Tên danh mục</label>
+                              <input type="text" class="form-control" name="name" placeholder="Nhập tên danh mục">
+                          </div>
+
+                          <div class="form-group">
+                              <label>Chọn danh mục cha</label>
+                              <select class="form-control" name="parent_id">
+                                  <option value="0">Chọn danh mục cha</option>
+                                  {!! $htmlOption !!}}
+                              </select>
+                          </div>
+                          <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                  </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
+
+
